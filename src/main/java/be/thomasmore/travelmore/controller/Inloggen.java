5d967cc4fa3f63a83c63aacfc4df9ae4c713640e
WouterDeLaet <wouter@sommico.com>
2018-11-28 11:24:02 +0100

    gebruikers beheren af

--- src/main/java/be/thomasmore/travelmore/controller/Inloggen.java
+++ src/main/java/be/thomasmore/travelmore/controller/Inloggen.java
@@ -50,6 +50,11 @@ public class Inloggen {
     public String logout() {
         HttpSession session = SessionUtils.getSession();
         session.invalidate();
+        System.err.println("FUBAR");
         return "uitgelogged";
+    }
+
+    public String backHome(){
+        return "index";
     }
 }
